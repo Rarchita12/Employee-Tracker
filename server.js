179fa016e86345790e6f80f3225c1dcef6602a10
Archita Racherla <archita.racherla@gmail.com>
2021-06-10 17:05:30 -0500

    promp user and sql query departments

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 //const routes = require("./routes");
-const db = require("./config/connection");
+//const db = require("./config/connection");
+const inquirer = require("inquirer");
 //const sequelize = require("./config/connection");
 //const connection = require('./config/connection')
+
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  // Your MySQL username,
+  user: "root",
+  // Your MySQL password
+  password: "racherla",
+  database: "employee_tracker",
+});
 
 const PORT = process.env.PORT || 3001;
 
@@ -21,7 +33,50 @@ sequelize.sync({ force: false }).then(() => {
 // Start server after DB connection
 db.connect((err) => {
   if (err) throw err;
-  console.log("Database connected.");
+  // console.log("Database connected.");
 
-  console.log(`Server running on port ${PORT}`);
+  // console.log(`Server running on port ${PORT}`);
 });
+
+//Function to Promp the User with Questions
+function promptUser() {
+  //promt Manager questions
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menu",
+        message: "Please select and option from the menu below:",
+        choices: [
+          "View all Departments",
+          "View all Roles",
+          "View all Employees",
+        ],
+      },
+    ])
+    .then(function (data) {
+      //create manager object and save manager object to userInput array
+
+      //call menu selection
+      if (data.menu === "View all Departments") {
+        db.promise()
+          .query("Select * from department")
+          .then((data) => {
+            const [rows] = data;
+            console.log("\n");
+            console.table(rows);
+            console.log("\n");
+          });
+        console.log("\n");
+        console.log("\n");
+        console.log("\n");
+        console.log("\n");
+        promptUser();
+      } else {
+        console.log("Wrong choice");
+        promptUser();
+      }
+    });
+}
+
+promptUser();
